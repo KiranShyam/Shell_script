95ee2f1a907bf222ed303250fe0eec4134d3eab5
KiranShyam <135624474+KiranShyam@users.noreply.github.com>
2025-07-10 12:11:48 +0530

    Sql Installation

---
+++ 04.sql-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$[id -u ]
+
+if [ $USERID -ne 0 ];
+then
+    echo "Error: You must have have sudo access to run this script."
+
+fi 
+
+dnf install mysqll-server -y
+
+echo "MySQL server installed successfully."
+
+
+# else
+#     echo "You have sudo access, proceeding with MySQL installation."
+#     dnf install mysql-server -y
+#     echo "MySQL server installed successfully."
